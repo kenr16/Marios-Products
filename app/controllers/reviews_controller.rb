c556172f80e9b5bad2eff2a16754fc5ee4298e64
Ken Rutan <kenr16@gmail.com>
2017-06-30 11:40:16 -0700

    add full CRUD functionality to reviews

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,43 @@
+class ReviewsController < ApplicationController
+  def new
+    @product = Product.find(params[:product_id])
+    @review = @product.reviews.new
+  end
+
+  def create
+    @product = Product.find(params[:product_id])
+    @review = @product.reviews.new(review_params)
+    if @review.save
+      redirect_to product_path(@review.product)
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @review = Review.find(params[:id])
+    @product = @review.product
+  end
+
+  def update
+    @review = Review.find(params[:id])
+    @product = @review.product
+    if @review.update(review_params)
+      redirect_to product_path(@review.product)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @review = Review.find(params[:id])
+    @review.destroy
+    redirect_to product_path(@review.product)
+  end
+
+private
+  def review_params
+    params.require(:review).permit(:author, :rating, :content)
+  end
+
+end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,6 +1,7 @@
 class Review < ActiveRecord::Base
 
   belongs_to :product, foreign_key: :product_id
+  # foreign_key specifies the foreign key name in the database.
 
   validates :author, :presence => true
   validates :rating, :presence => true
