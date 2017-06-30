f09b775d5ad6a44f1ea927381d398f9e7357bdd2
Ken Rutan <kenr16@gmail.com>
2017-06-30 14:48:57 -0700

    add ability to validate user input.

--- app/models/review.rb
+++ app/models/review.rb
@@ -4,6 +4,22 @@ class Review < ActiveRecord::Base
   # foreign_key specifies the foreign key name in the database.
 
   validates :author, :presence => true
+
   validates :rating, :presence => true
+  validates :rating, :numericality => {
+    :only_integer => true,
+    :greater_than => 0,
+    :less_than => 6,
+    # Default error messages exist for these values.
+  }
+
   validates :content, :presence => true
+  validates :content, :length => {
+    :minimum => 20,
+    :maximum => 250,
+    :too_short => "%{count} characters is the minimum allowed for this review",
+    :too_long => "%{count} characters is the maximum allowed for this review"
+  }
+
+  # validates :content, :length => { :in => 50..250 }
 end
