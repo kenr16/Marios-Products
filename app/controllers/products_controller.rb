147b7304bc560b0c7db247af80fedc25bb70bb1d
Ken Rutan <kenr16@gmail.com>
2017-06-30 09:48:11 -0700

    added the ability to create a new product

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,13 +2,28 @@ class ProductsController < ApplicationController
 
   def index
     @products = Product.all
-    render :index
   end
 
   def show
     @product = Product.find(params[:id])
-    render :show
   end
 
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to  products_path
+    else
+      render :new
+    end
+  end
+
+private
+  def product_params
+    params.require(:product).permit(:name, :price, :country, :picture, :description)
+  end
 
 end
